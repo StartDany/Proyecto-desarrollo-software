a7cba6c2c762f4bc5e6b4dfbca5c8dddbecb237b
Infante y Ortiz <danyjuegos11@gmail.com>
2026-08-31 14:04:45 -0500

    Crea programa principal

---
+++ main.py
@@ -0,0 +1,7 @@
+print("====================================")
+print("PROYECTO DE DESARROLLO DE SOFTWARE")
+print("====================================")
+
+print("Bienvenido al proyecto")
+print("Estudiantes:Infante Luis y Ortiz Andres")
+print("Curso: 10A")
